443109cf61dfdaa2da16aa128e9be812b708fc35
rleite-matrix <rafael.leite@matrixenergia.com>
2025-04-21 13:42:38 -0300

    fix: ajuste docker-compose redis

--- src/main/java/com/simplesdental/product/infrastructure/dto/UserContextOutput.java
+++ src/main/java/com/simplesdental/product/infrastructure/dto/UserContextOutput.java
@@ -3,8 +3,10 @@ package com.simplesdental.product.infrastructure.dto;
 import com.simplesdental.product.domain.model.field.Role;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class UserContextOutput {
+public class UserContextOutput implements Serializable {
     private Long id;
     private String email;
     private Role role;
